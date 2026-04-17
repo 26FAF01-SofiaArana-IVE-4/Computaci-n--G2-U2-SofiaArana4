559a5bbdb28e4cf6c1dd02a8e28a7df1897c7a2c
26FAF01-SofiaArana-IVE-4 <aranafrida16db@gmail.com>
2026-04-17 13:08:16 -0600

    Add files via upload

---
+++ CalculadoraBasica4sofiaarana/src/main/java/com/mycompany/calculadorabasica4sofiaarana/CalculadoraBasica4sofiaarana.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadorabasica4sofiaarana;
+
+/**
+ *
+ * @author LABORATORIO
+ */
+public class CalculadoraBasica4sofiaarana {
+
+    public static void main(String[] args) {
+        jfrmCalc HolaPos=new jfrmCalc();
+        HolaPos.setVisible(true);
+    }
+}
